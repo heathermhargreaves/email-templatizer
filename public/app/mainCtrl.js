09e9b6ce0d53eb1d236f8cbe2a83f40821b4c640
heathermhargreaves <heathermhargreaves@gmail.com>
2016-10-07 16:22:06 -0700

    bug fix, now when user submits different territory and industry, old arrays get cleared and filled with new info

--- public/app/mainCtrl.js
+++ public/app/mainCtrl.js
@@ -11,6 +11,9 @@ angular.module('app')
     $scope.customerSnippets = [];
     $scope.industry //selected from industry options
     $scope.territory //selected from territory options
+    $scope.itemsInIndustryArray = false //ng-show for industry snippets
+    $scope.itemsInCustomerArray = false //ng-show for industry snippets
+
 
     //data from service
     $scope.companyData = mainSrvc.companyData;
@@ -26,22 +29,31 @@ angular.module('app')
 
 
     $scope.filterCompanyData = function() {
+      if($scope.customerSnippets.length > 0) {
+        $scope.customerSnippets = [];
+      }
       $scope.companyData.filter(function(obj) {
           if(obj.territory === $scope.territory) {
-            $scope.customerSnippets.push(obj)
-            console.log($scope.customerSnippets);
+            $scope.customerSnippets.push(obj);
           }
         })
-    }
+      if($scope.customerSnippets.length > 0) {
+        $scope.itemsInCustomerArray = true;
+      }
+    };
 
     $scope.filterIndustryData = function() {
+      if($scope.industrySnippets.length > 0) {
+        $scope.industrySnippets = [];
+        console.log($scope.industrySnippets);
+      }
       $scope.industryData.filter(function(obj) {
           if(obj.industry === $scope.industry) {
-            $scope.industrySnippets.push(obj)
+            $scope.industrySnippets.push(obj);
             console.log($scope.industrySnippets);
           }
-        })
-    }
+        });
+    };
 
 
   }); //end mainCtrl
